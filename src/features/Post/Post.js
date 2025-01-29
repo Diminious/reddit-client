767be8fa804d797fe251faed97e43b2e377447a6
Diminious <matthewzengoski@gmail.com>
2025-01-29 21:45:38 +1100

    Added link support for media

--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown'
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime'
 import './Post.css'
@@ -7,16 +8,32 @@ dayjs.extend(relativeTime);
 const Post = (props) => {
     const {post, onClickComments} = props;
 
+    const displayMedia = (mediaPost) => {
+        switch (mediaPost.post_hint) {
+            case "image":
+                return (
+                    <div className="post-media-container">
+                        <img src={mediaPost.url} alt="post" className="post-media"/>
+                    </div>
+                )
+            
+            default: 
+                return (
+                    <div className="post-media-container">
+                        <a href={mediaPost.url} target="blank" alt="link" className="post-media">Link to media</a>
+                    </div>
+                )
+        }
+    }
+
     return (
         <article key={post.id} onClick={() => onClickComments(post)}>
                 <div className="post-container">
                     <h3>{post.title}</h3>
                     <div className="post-text-container">
-                        <p className="post-text">{post.bodyText}</p>
+                        <ReactMarkdown className="post-text" children={post.selftext} />
                     </div>
-                    <div className="post-image-container">
-                        <img src={post.url} alt="post" className="post-image"/>
-                    </div>
+                    {displayMedia(post)}
                 </div>
 
                 <div className="post-details">
